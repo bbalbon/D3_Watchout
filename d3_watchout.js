6dbbd7f7c3062311297b3fbe9229b837ff2753da
Bryce Balbon <bbalbon@gmail.com>
2019-12-28 16:53:54 -0800

    Render enemies and give random movement

--- d3_watchout.js
+++ d3_watchout.js
@@ -1,46 +1,60 @@
-let testArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'];
-
-// d3.select('.main').selectAll('svg')
-//     .data(testArray)
-//     .enter()
-//     .append('svg')
-//     .attr('width', '50')
-//     .attr('height', '50')
-//     .attr('class', 'circle')
-
-
+let testData = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const width = 750;
 const height = 600;
+
 //Create SVG element
 let svg = d3.select('.svgcontainer')
     .append('svg')
     .attr('width', width)
     .attr('height', height);
 
+let mappedData = testData.map(item => {
+    return {
+        value: item,
+        x: Math.random() * 700,
+        y: Math.random() * 550
+    }
+})
+
+svg.selectAll('svg')
+    .data(mappedData)
+    .enter()
+    .append('rect')
+    .attr('x', function(data) {
+        return data.x;
+    })
+    .attr('y', function(data) {
+        return data.y;
+    })
+    .attr('width', 25)
+    .attr('height', 25)
+    .attr('fill', 'indianred')
+    .attr('stroke', 'black');
+
+
 function update() {
 
-    svg.selectAll('svg')
-        .data(testArray)
-        .text((d) => {return d})
-        .enter()
-        .append('rect')
-        .attr('x', `${Math.random() * 700}`)
-        .attr('y', `${Math.random() * 550}`)
-        .attr('width', 25)
-        .attr('height', 25)
-        .attr('fill', 'indianred')
-        .attr('stroke', 'black');
-    
-    // svg.selectAll('rect')
-    //     .data([])
-    //     .exit()
-    //     .remove()
+    let newMappedData = mappedData.map(item => {
+        item.x = Math.random() * 700
+        item.y = Math.random() * 550
+        return item;
+    })
+
+    d3.select('svg').selectAll('rect')
+        .data(newMappedData)
+        .transition().duration(1500)
+        .attr('x', function(data) {
+            return data.x;
+        })
+        .attr('y', function(data) {
+            return data.y
+        })
 
 }
 
-// d3.interval(() => {
-//     update();
-// }, 3000);
+d3.interval(() => {
+    update();
+}, 2000);
 
 
 // const width = 50;
@@ -52,11 +66,3 @@ function update() {
 //    .attr('height', height);
 
 // console.log(svg);
-
-// svg.append('circle')
-//     .attr('cx', `25`)
-//     .attr('cy', `25`)
-//     .attr('radius', '18')
-//     .attr('stroke', 'black')
-//     .attr('stroke-width', '2.5')
-//     .attr('fill', 'indianred')
